9f34952beb667bbb70d9458a0065b938bfbc61b6
Heather Brysiewicz <h.a.brysiewicz@gmail.com>
2016-07-26 10:05:28 -0700

    Initial commit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,24 +1,33 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var envTarget = process.env.DEPLOY_TARGET || EmberApp.env();
+var ENV = require('./config/environment.js')(envTarget);
+var isProductionLikeBuild = ['production', 'staging'].indexOf(envTarget) > -1;
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    fingerprint: {
+      enabled: isProductionLikeBuild,
+      generateAssetMap: true,
+      replaceExtensions: ['html', 'js', 'css'],
+      prepend: `${ENV.assetHost}/`
+    },
+    sourcemaps: { enabled: isProductionLikeBuild },
+    minifyCSS: { enabled: isProductionLikeBuild },
+    minifyJS: { enabled: isProductionLikeBuild },
+    tests: process.env.EMBER_CLI_TEST_COMMAND || !isProductionLikeBuild,
+    hinting: process.env.EMBER_CLI_TEST_COMMAND || !isProductionLikeBuild,
+    postcssOptions: {
+      compile: {
+        plugins: [
+          {
+            module: require('narwin-pack')
+          }
+        ]
+      }
+    }
   });
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
 
   return app.toTree();
 };
